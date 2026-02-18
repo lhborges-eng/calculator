bb1067b3b564d99213bd5941d244a17af812ee85
Luiz Borges <luizikike@gmail.com>
2026-02-18 01:51:32 -0300

    Add basic calculator logic in script.js
    Implement basic calculator functionality with operators and clear.

---
+++ script.js
@@ -0,0 +1,61 @@
+let runningResult = 0;
+let buffer = "0";
+let previusOperator;
+
+const screen = document.querySelector('.screen');
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+    }else{
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'C':
+        buffer = '0';
+        runningResult = 0;
+        break;
+    case '=':
+        if(previusOperator === null)
+            return
+        flushOperator(parseInt(buffer));
+        previusOperator = null;
+        buffer = runningResult;
+        runningResult = 0;
+        break;
+    case '←':
+        if(buffer.length ===1){
+            buffer = '0';
+        } else {
+            buffer = buffer.slice(0, buffer.length - 1);
+        }
+        break;
+    case '+':
+    case '-':
+    case '×':
+    case '÷':
+        handleMath(symbol);
+        break;
+    }
+}
+
+function handleMath(symbol){
+    if(buffer === '0'){
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningResult === 0){
+        runningResult = intBuffer;
+    } else {
+        flushOperator(intBuffer);
+    }
+    previusOperator =  symbol;
+    buffer = '0';
+}
+
+//function flushOperator()
